0780eb1eebaa13c1c06053e60956a3d37f2a0168
TylerPete <tpeterson4@byupathway.edu>
2025-07-04 17:56:09 -0500

    finished header code-along activity

---
+++ code-along-header/scripts/navigation.js
@@ -0,0 +1,6 @@
+const navbutton = document.querySelector("#ham-btn");
+
+//Toggle the show class on and off
+navbutton.addEventListener("click", () => {
+    navbutton.classList.toggle("show");
+});
