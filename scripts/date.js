51cbc0cf6549f14038e7a61e79daa7971c93b55c
TylerPete <tpeterson4@byupathway.edu>
2025-07-08 18:05:11 -0500

    fixed last modified date formatting

--- scripts/date.js
+++ scripts/date.js
@@ -6,4 +6,7 @@ today = new Date();
 lastModifiedDate = document.lastModified;
 
 year.textContent = today.getFullYear();
-modifiedParagraph.textContent = `Last Modified: ${lastModifiedDate}`;
+modifiedParagraph.textContent = `Last Modified: ${new Intl.DateTimeFormat("en-US",
+    {
+        dateStyle: "short", timeStyle: "medium"
+    }).format(lastModifiedDate)}`;
