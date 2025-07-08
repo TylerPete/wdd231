c8432d481e4e000980336f3ec1b40500423ada31
TylerPete <tpeterson4@byupathway.edu>
2025-07-08 18:01:56 -0500

    wrote javascript for footer year and modified date

--- scripts/date.js
+++ scripts/date.js
@@ -1,5 +1,9 @@
 const year = document.querySelector("#currentyear");
+const modifiedParagraph = document.querySelector("#lastModified");
 
 today = new Date();
 
+lastModifiedDate = document.lastModified;
+
 year.textContent = today.getFullYear();
+modifiedParagraph.textContent = `Last Modified: ${lastModifiedDate}`;
