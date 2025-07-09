dcd4a74c1e0f8058d62dc45d97f1879c325203c9
TylerPete <tpeterson4@byupathway.edu>
2025-07-09 17:44:23 -0500

    more progress

--- scripts/navigation.js
+++ scripts/navigation.js
@@ -1,1 +1,7 @@
-//meow
+const navBar = document.querySelector("#nav-bar");
+const hamButton = document.querySelector("#ham-btn");
+
+hamButton.addEventListener("click", () => {
+    navBar.classList.toggle("show");
+    hamButton.classList.toggle("show");
+});
