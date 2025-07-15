cd12c854d266bccc4321ac471e60e3e8893cad77
TylerPete <tpeterson4@byupathway.edu>
2025-07-15 17:07:43 -0500

    started laying out HTML and little bit of JavaScript for FetchAPI learning activity

--- scripts/prophets.js
+++ scripts/prophets.js
@@ -1,1 +1,6 @@
-//meow
+const url = "https://byui-cse.github.io/cse-ww-program/data/latter-day-prophets.json";
+const cards = document.querySelector("#cards");
+
+async function getProphetData() {
+    //code
+}
