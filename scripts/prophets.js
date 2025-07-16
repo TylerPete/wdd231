3b117898878a2d7d34c8351a726df1f1af769057
TylerPete <tpeterson4@byupathway.edu>
2025-07-16 16:25:28 -0500

    finished The Fetch API Learning activity with latter-day prophets (well enough, anyways. CSS could use a little work but it's alright)

--- scripts/prophets.js
+++ scripts/prophets.js
@@ -11,7 +11,28 @@ async function getProphetData() {
 
 const displayProphets = (prophets) => {
     prophets.forEach((prophet) => {
-        //card build code here
+        let card = document.createElement("section");
+        let fullName = document.createElement("h2");
+        let dateOfBirth = document.createElement("p");
+        let placeOfBirth = document.createElement("p");
+        let portrait = document.createElement("img");
+
+        fullName.textContent = `${prophet.name} ${prophet.lastname}`;
+
+        dateOfBirth.textContent = `Date of Birth: ${prophet.birthdate}`;
+        placeOfBirth.textContent = `Place of Birth: ${prophet.birthplace}`;
+
+        portrait.setAttribute("src", prophet.imageurl);
+        portrait.setAttribute("alt", `${fullName.textContent} portrait`);
+        portrait.setAttribute("loading", "lazy");
+        portrait.setAttribute("width", "340");
+        portrait.setAttribute("height", "440");
+
+        card.appendChild(fullName);
+        card.appendChild(dateOfBirth);
+        card.appendChild(placeOfBirth);
+        card.appendChild(portrait);
+        cards.appendChild(card);
     });
 }
 
