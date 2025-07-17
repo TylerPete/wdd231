306a71d74c9c8adc8aa5cc093e478d64f7085741
TylerPete <tpeterson4@byupathway.edu>
2025-07-17 16:23:38 -0500

    added JavaScripts for current year and last modified date/time

---
+++ chamber/scripts/directory.js
@@ -0,0 +1,11 @@
+const currentYear = document.querySelector("#current-year");
+const lastModified = document.querySelector("#last-modified");
+
+const today = new Date();
+currentYear.textContent = today.getFullYear();
+
+const lastModifiedDate = new Date(document.lastModified);
+lastModified.textContent = `${new Intl.DateTimeFormat("en-US",
+    {
+        dateStyle: "short", timeStyle: "medium"
+    }).format(lastModifiedDate)}`;
