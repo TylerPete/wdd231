670a1ac6c5b26b60298fbf60446c3dd9b29bc4e7
TylerPete <tpeterson4@byupathway.edu>
2025-07-17 19:50:54 -0500

    don't remember what i did most recently

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -1,6 +1,8 @@
 const currentYear = document.querySelector("#current-year");
 const lastModified = document.querySelector("#last-modified");
 
+
+
 const today = new Date();
 currentYear.textContent = today.getFullYear();
 
