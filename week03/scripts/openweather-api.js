f54c1e52d9e17d2e62816f4ba721f4a9814ed01f
TylerPete <tpeterson4@byupathway.edu>
2025-07-23 16:58:34 -0500

    finished consuming an API learning activity (screw the CSS)

--- week03/scripts/openweather-api.js
+++ week03/scripts/openweather-api.js
@@ -1,6 +1,6 @@
 const currentTempInTrier = document.querySelector("#current-temp");
 const weatherIconImg = document.querySelector("#weather-icon");
-const figCaption = document.querySelector("figcaption");
+const figCaptionDescription = document.querySelector("figcaption");
 
 const url = "https://api.openweathermap.org/data/2.5/weather?lat=49.75&lon=6.64&units=imperial&appid=1e39b8f71c9501c6b14ff392dc8ac7b4";
 
@@ -10,7 +10,7 @@ async function apiFetch() {
         if (response.ok) {
             const data = await response.json();
             console.log(data);
-            //displayResults(data);
+            displayResults(data);
         } else {
             throw Error(await response.text());
         }
@@ -20,3 +20,12 @@ async function apiFetch() {
 }
 
 apiFetch();
+
+function displayResults(data) {
+    const temp = `${data.main.temp}&deg;F`;
+    currentTempInTrier.innerHTML = temp;
+    let description = data.weather[0].description;
+    figCaptionDescription.textContent = description;
+    weatherIconImg.src = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+    weatherIconImg.alt = description;
+}
