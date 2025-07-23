c191c4205414509d2c54fc5087179db1b837f148
TylerPete <tpeterson4@byupathway.edu>
2025-07-23 15:46:36 -0500

    started JavaScript file for openweather API calls and put together a query string URL thing based on the activity instructions

---
+++ week03/scripts/openweather-api.js
@@ -0,0 +1,5 @@
+const currentTempInTrier = document.querySelector("#current-temp");
+const weatherIconImg = document.querySelector("#weather-icon");
+const figCaption = document.querySelector("figcaption");
+
+const url = "https://api.openweathermap.org/data/2.5/weather?lat=49.75&lon=6.64&units=imperial&appid=1e39b8f71c9501c6b14ff392dc8ac7b4";
