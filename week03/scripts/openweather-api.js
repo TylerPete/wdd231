2da3b547915f2652e3467e2d262ecba4f09331d1
TylerPete <tpeterson4@byupathway.edu>
2025-07-23 15:59:52 -0500

    'bout to run it locally

--- week03/scripts/openweather-api.js
+++ week03/scripts/openweather-api.js
@@ -3,3 +3,20 @@ const weatherIconImg = document.querySelector("#weather-icon");
 const figCaption = document.querySelector("figcaption");
 
 const url = "https://api.openweathermap.org/data/2.5/weather?lat=49.75&lon=6.64&units=imperial&appid=1e39b8f71c9501c6b14ff392dc8ac7b4";
+
+async function apiFetch() {
+    try {
+        const response = await fetch(url);
+        if (response.ok) {
+            const data = await response.json();
+            console.log(data);
+            //displayResults(data);
+        } else {
+            throw Error(await response.text());
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+apiFetch();
