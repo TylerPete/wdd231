075dff5daf94406f3cf15a97f05c8ef3024deea1
TylerPete <tpeterson4@byupathway.edu>
2025-07-27 18:46:01 -0500

    trying to fix empty alt and empty src attributes for weather-icon img element to be practically immediately set by JS

--- chamber/scripts/index.js
+++ chamber/scripts/index.js
@@ -85,7 +85,8 @@ function displayCurrentResults(data) {
 
     riseElement.textContent = `Sunrise: ${sunriseDate.toLocaleTimeString().slice(0, -6)} ${sunriseDate.toLocaleTimeString().slice(-2)}`;
     setElement.textContent = `Sunset: ${sunsetDate.toLocaleTimeString().slice(0, -6)} ${sunsetDate.toLocaleTimeString().slice(-2)}`;
-    imgElement.src = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+    imgElement.setAttribute("src", `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`);
+    imgElement.setAttribute("alt", data.weather[0].description);
 }
 
 async function forecastApiFetch() {
