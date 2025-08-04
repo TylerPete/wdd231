4af703564467a8f6d9ff51a17418c1ee3c2f9663
TylerPete <tpeterson4@byupathway.edu>
2025-08-04 14:36:13 -0500

    adding javascript to open the modals

--- chamber/scripts/join.js
+++ chamber/scripts/join.js
@@ -35,3 +35,24 @@ submitButton.addEventListener("click", () => {
 
     hiddenTimestampInput.value = dateTimeNowString;
 });
+
+
+//Open the appropriate modals for details about membership levels when "Learn more" buttons are clicked
+
+const goldModal = document.querySelector("#gold-modal");
+
+document.querySelector("#np-level-button").addEventListener("click", () => {
+    document.querySelector("#np-modal").showModal();
+});
+
+document.querySelector("#bronze-level-button").addEventListener("click", () => {
+    document.querySelector("#bronze-modal").showModal();
+});
+
+document.querySelector("#silver-level-button").addEventListener("click", () => {
+    document.querySelector("#silver-modal").showModal();
+});
+
+document.querySelector("#gold-level-button").addEventListener("click", () => {
+    document.querySelector("#gold-modal").showModal();
+});
