8c4060c7292575a84aa1cff2733cd4f91ac95bcb
TylerPete <tpeterson4@byupathway.edu>
2025-08-04 13:30:18 -0500

    referenced the script

--- chamber/scripts/join.js
+++ chamber/scripts/join.js
@@ -1,1 +1,19 @@
-//meow
+const currentYear = document.querySelector("#current-year");
+const lastModified = document.querySelector("#last-modified");
+
+const today = new Date();
+currentYear.textContent = today.getFullYear();
+
+const lastModifiedDate = new Date(document.lastModified);
+lastModified.textContent = `${new Intl.DateTimeFormat("en-US",
+    {
+        dateStyle: "short", timeStyle: "medium"
+    }).format(lastModifiedDate)}`;
+
+// Navigation/hamburger button functionality
+const navBar = document.querySelector("#nav-bar");
+const hamburger = document.querySelector("#ham-btn");
+
+hamburger.addEventListener("click", () => {
+    navBar.classList.toggle("show");
+});
