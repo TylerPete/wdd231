f7eeb126ea6fc5e5b2ebfd362053b49b8100e3cf
TylerPete <tpeterson4@byupathway.edu>
2025-08-11 18:33:14 -0500

    fixed stupid white background issue

---
+++ finalproject/scripts/retirement-hub.mjs
@@ -0,0 +1,8 @@
+import { setCopyrightYear } from "./copyright-year.mjs";
+import { setUpNavigation } from "./navigation.mjs";
+
+//Input current year for copyright year
+setCopyrightYear();
+
+//Make hamburger button navigation function
+setUpNavigation();
