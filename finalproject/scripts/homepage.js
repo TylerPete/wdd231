03adbc897e97b8c40ac5315c04f9f4a3ab0e9528
TylerPete <tpeterson4@byupathway.edu>
2025-08-11 15:29:49 -0500

    FINISHED all content and styling for mobile screen-sized homepage; larger screen size styling, as well as href attribute ACTUAL links and the meta og:image element TBD later.

--- finalproject/scripts/homepage.js
+++ finalproject/scripts/homepage.js
@@ -1,3 +1,4 @@
+//Make hamburger button navigation function
 const navBar = document.querySelector("#nav-bar");
 const hamButton = document.querySelector("#ham-btn");
 
@@ -5,3 +6,8 @@ hamButton.addEventListener("click", () => {
     navBar.classList.toggle("show");
     hamButton.classList.toggle("show");
 });
+
+//Input current year for copyright year
+const year = document.querySelector("#currentyear");
+today = new Date();
+year.textContent = today.getFullYear();
