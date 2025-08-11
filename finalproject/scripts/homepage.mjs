b698ae3a54ecf558b53c6c8d6802146afe82c4e1
TylerPete <tpeterson4@byupathway.edu>
2025-08-11 18:22:21 -0500

    fixed some background-color properties of stuff on the homepage (needed transparency)

--- finalproject/scripts/homepage.mjs
+++ finalproject/scripts/homepage.mjs
@@ -1,13 +1,8 @@
 import { setCopyrightYear } from "./copyright-year.mjs";
-
-//Make hamburger button navigation function
-const navBar = document.querySelector("#nav-bar");
-const hamButton = document.querySelector("#ham-btn");
-
-hamButton.addEventListener("click", () => {
-    navBar.classList.toggle("show");
-    hamButton.classList.toggle("show");
-});
+import { setUpNavigation } from "./navigation.mjs";
 
 //Input current year for copyright year
 setCopyrightYear();
+
+//Make hamburger button navigation function
+setUpNavigation();
---
+++ finalproject/scripts/navigation.mjs
@@ -0,0 +1,10 @@
+//Make hamburger button navigation function
+export function setUpNavigation() {
+    const navBar = document.querySelector("#nav-bar");
+    const hamButton = document.querySelector("#ham-btn");
+
+    hamButton.addEventListener("click", () => {
+        navBar.classList.toggle("show");
+        hamButton.classList.toggle("show");
+    });
+}
