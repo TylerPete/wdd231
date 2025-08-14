1ee2fac98c3c491abeade17ae2c6485257c3ff00
TylerPete <tpeterson4@byupathway.edu>
2025-08-13 21:36:43 -0500

    added hidden timestamp input

--- finalproject/scripts/debt-payoff-tools.mjs
+++ finalproject/scripts/debt-payoff-tools.mjs
@@ -1,8 +1,25 @@
 import { setCopyrightYear } from "./copyright-year.mjs";
 import { setUpNavigation } from "./navigation.mjs";
 
+const submitButton = document.querySelector("#submit-button");
+const hiddenTimestampInput = document.querySelector("#timestamp");
+
 //Input current year for copyright year
 setCopyrightYear();
 
 //Make hamburger button navigation function
 setUpNavigation();
+
+submitButton.addEventListener("click", () => {
+    const dateTimeNow = new Date();
+    let options = {
+        month: "long",
+        day: "numeric",
+        year: "numeric",
+        hour: "numeric",
+        minute: "numeric"
+    }
+    const dateTimeNowString = new Intl.DateTimeFormat("en-us", options).format(dateTimeNow);
+
+    hiddenTimestampInput.value = dateTimeNowString;
+});
